749349c9abc6bfa141b581c1f57459e600c50fe6
Cyril Plisko <cyril.plisko@mountall.com>
2022-02-15 23:07:41 +0200

    Update doccomment

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
+//!
+//!
+//! Popular alphabets for use with `nanoid::nanoid!()` macro
+//!
+//! ```
+//! use nanoid_dictionary::NOLOOKALIKES;
+//! use nanoid::nanoid;
+//!
+//! let id = nanoid!(21, NOLOOKALIKES);
+//! assert_eq!(id.len(), 21);
+//! println!("{}", id);
+//!```
+
 /// Standard NanoId alphabet (64 symbols)
 /// English lowercase letters, uppercase letters, digits, and symbols '_' and '-'.
+///
 pub const ALPHABET_STD: &[char] = &[
     '_', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
     'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -8,32 +22,38 @@ pub const ALPHABET_STD: &[char] = &[
 ];
 
 /// Lowercase English letters: abcdefghijklmnopqrstuvwxyz
+///
 pub const LOWERCASE: &[char] = &[
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
 /// Uppercase English letters: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+///
 pub const UPPERCASE: &[char] = &[
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
 
 /// Numbers from 0 to 9
+///
 pub const NUMBERS: &[char] = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-/// Lowercase English hexadecimal lowercase characters: 0123456789abcdef
+/// Lowercase English hexadecimal characters: 0123456789abcdef
+///
 pub const HEXADECIMAL_LOWERCASE: &[char] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
 ];
 
-/// Lowercase English hexadecimal uppercase characters: 0123456789ABCDEF
+/// Uppercase English hexadecimal characters: 0123456789ABCDEF
+///
 pub const HEXADECIMAL_UPPERCASE: &[char] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
 ];
 
 /// Combination of all the lowercase, uppercase characters and numbers from 0 to 9
 /// Does not include any symbols or special characters
+///
 pub const ALPHANUMERIC: &[char] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
@@ -43,6 +63,7 @@ pub const ALPHANUMERIC: &[char] = &[
 
 /// Numbers and english alphabet without lookalikes: 1, l, I, 0, O, o, u, v, 5, S, s, 2, Z.
 /// Complete set: 346789ABCDEFGHJKLMNPQRTUVWXYabcdefghijkmnpqrtwxyz
+///
 pub const NOLOOKALIKES: &[char] = &[
     '3', '4', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
     'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
@@ -52,6 +73,7 @@ pub const NOLOOKALIKES: &[char] = &[
 /// Same as noolookalikes but with removed vowels and following letters: 3, 4, x, X, V.
 /// This list should protect you from accidentally getting obscene words in generated strings.
 /// Complete set: 6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz
+///
 pub const NOLOOKALIKES_SAFE: &[char] = &[
     '6', '7', '8', '9', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'T',
     'W', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 't', 'w', 'z',
