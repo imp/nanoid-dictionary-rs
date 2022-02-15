2e183e26a37f6118780b98be73f467489d752ed5
Cyril Plisko <cyril.plisko@mountall.com>
2022-02-15 16:59:46 +0200

    Add alphabet doccomments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+/// Standard NanoId alphabet (64 symbols)
+/// English lowercase letters, uppercase letters, digits, and symbols '_' and '-'.
 pub const ALPHABET_STD: &[char] = &[
     '_', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
     'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -5,26 +7,33 @@ pub const ALPHABET_STD: &[char] = &[
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
 
+/// Lowercase English letters: abcdefghijklmnopqrstuvwxyz
 pub const LOWERCASE: &[char] = &[
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
+/// Uppercase English letters: ABCDEFGHIJKLMNOPQRSTUVWXYZ
 pub const UPPERCASE: &[char] = &[
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
 
+/// Numbers from 0 to 9
 pub const NUMBERS: &[char] = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+/// Lowercase English hexadecimal lowercase characters: 0123456789abcdef
 pub const HEXADECIMAL_LOWERCASE: &[char] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
 ];
 
+/// Lowercase English hexadecimal uppercase characters: 0123456789ABCDEF
 pub const HEXADECIMAL_UPPERCASE: &[char] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
 ];
 
+/// Combination of all the lowercase, uppercase characters and numbers from 0 to 9
+/// Does not include any symbols or special characters
 pub const ALPHANUMERIC: &[char] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
     'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
@@ -32,6 +41,8 @@ pub const ALPHANUMERIC: &[char] = &[
     'V', 'W', 'X', 'Y', 'Z',
 ];
 
+/// Numbers and english alphabet without lookalikes: 1, l, I, 0, O, o, u, v, 5, S, s, 2, Z.
+/// Complete set: 346789ABCDEFGHJKLMNPQRTUVWXYabcdefghijkmnpqrtwxyz
 pub const NOLOOKALIKES: &[char] = &[
     '3', '4', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N',
     'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
